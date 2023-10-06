1988e505836c69e34cc1f7f89a593d03dc6d8a04
Muneeb <muneebafzal381a@gmail.com>
2023-10-06 11:27:31 +0500

    update the header section and profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 
 
@@ -17,7 +18,9 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/sign-in' element={<SignIn />}></Route>
         <Route path='/sign-up' element={<SignUp />}></Route>
-        <Route path='/profile' element={<Profile />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Route>
         <Route path='/about' element={<About />}></Route>
       </Routes>
     </BrowserRouter>
